eb5c72c4e6309cb9c8800b0d415f89bf019e26a9
Mr Greg <gregsw123@gmail.com>
2022-03-05 16:47:53 -0600

    Prueba 1

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function consumirAPI()
+    {
+        $cliente1 = new \GuzzleHttp\Client();
+        $response = $cliente1->request('GET', 'https://zoo-animal-api.herokuapp.com/animals/rand/10');
+        $Animales = json_decode($response->getBody()->getContents(), true);
+
+        return($Animales); 
+    }
+
+
+    public function index(){
+        return view('index', ['Animales'=>$this->consumirAPI()]);
+    }
 }
