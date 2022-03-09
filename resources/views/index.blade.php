a390e15cf08b4135b84951f0c1d0e99cb0ebeff3
FernandoHG07 <lfhernandezg@outlook.com>
2022-03-09 13:45:48 -0600

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,9 @@
         <div class="hero-full-wrapper">
           <div class="text-content">
             <h1>DinoAnimales</h1>
-            <p>Quieres saber algunas cosas acerca de Animales y Dinosaurios?</p>
-            <p>Estas en la pagina correcta.</p>
-            <p>VAMOS!, dale a las 3 lineas de arriba y empieza.</p>
+            <h3>Quieres saber algunas cosas acerca de Animales y Dinosaurios?</h3>
+            <h3>Estas en la pagina correcta.</h3>
+            <h3>VAMOS!, dale a las 3 lineas de arriba y empieza.</h3>
           </div>
         </div>
       </div>
