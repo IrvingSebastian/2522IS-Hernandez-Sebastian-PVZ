f0e5d539e32f787a71a8304147113181f9c79edf
Mr Greg <gregsw123@gmail.com>
2022-03-11 11:51:17 -0600

    Discord 3

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -74,8 +74,8 @@
                 </div>
             </div>
         </div>
-        
-        <iframe src="https://discord.com/widget?id=858003129064751114&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+
+        <iframe style="align-items: center" src="https://discord.com/widget?id=858003129064751114&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     </div>
 </div>
 @endsection
